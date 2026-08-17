85d45a3a4c4bfee531f3d5fc9008559b5481b86e
Maler2 <testhapusakuns@gmail.com>
2026-08-17 20:46:57 +0700

    20:46:57 17-08-2026 - fix dpad dan tambahkan indikator saat dipencet

--- script/dpad.gd
+++ script/dpad.gd
@@ -3,16 +3,10 @@ extends TextureRect
 signal dpad_pressed(direction: String)
 signal dpad_released()
 
-@onready var btn_up: CanvasItem = $BtnUp
-@onready var btn_down: CanvasItem = $BtnDown
-@onready var btn_left: CanvasItem = $BtnLeft
-@onready var btn_right: CanvasItem = $BtnRight
-
 var active_direction: String = ""
 var touch_index: int = -1
 
-var color_normal: Color = Color(1, 1, 1, 0)
-var color_pressed: Color = Color(0, 0, 0, 0.4)
+var dir_map = { "": 0, "up": 1, "down": 2, "left": 3, "right": 4 }
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch:
@@ -35,26 +29,14 @@ func _input(event: InputEvent) -> void:
 						active_direction = "down"
 				
 				dpad_pressed.emit(active_direction)
-				_update_visuals(active_direction)
+				_update_shader()
 		else:
 			if event.index == touch_index:
 				touch_index = -1
 				active_direction = ""
 				dpad_released.emit()
-				_update_visuals("")
+				_update_shader()
 
-func _update_visuals(dir: String) -> void:
-	if btn_up: btn_up.modulate = color_normal
-	if btn_down: btn_down.modulate = color_normal
-	if btn_left: btn_left.modulate = color_normal
-	if btn_right: btn_right.modulate = color_normal
-	
-	match dir:
-		"up":
-			if btn_up: btn_up.modulate = color_pressed
-		"down":
-			if btn_down: btn_down.modulate = color_pressed
-		"left":
-			if btn_left: btn_left.modulate = color_pressed
-		"right":
-			if btn_right: btn_right.modulate = color_pressed
+func _update_shader() -> void:
+	if material and material is ShaderMaterial:
+		material.set_shader_parameter("active_dir", dir_map[active_direction])
